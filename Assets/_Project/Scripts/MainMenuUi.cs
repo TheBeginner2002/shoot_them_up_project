0b49a7ddef87915983376f1bf0bb237def472515
PhamQuyen27 <2051063534@e.tlu.edu.vn>
2024-01-03 02:00:20 +0700

    Fix bug item

--- Assets/_Project/Scripts/MainMenuUi.cs
+++ Assets/_Project/Scripts/MainMenuUi.cs
@@ -8,16 +8,13 @@ namespace Shmup
         public class MainMenuUi : MonoBehaviour
         {
             [SerializeField] SceneReference startingLevel;
-            [SerializeField] SceneReference settingScene;
             [SerializeField] Button playButton;
             [SerializeField] Button quitButton;
-            [SerializeField] Button settingButton;
 
-        private void Awake()
+            private void Awake()
             {
                 playButton.onClick.AddListener(() => Loader.Load(startingLevel));
                 quitButton.onClick.AddListener(() => Helpers.Quitgame());
-                settingButton.onClick.AddListener(() => Loader.Load(settingScene));
                 Time.timeScale = 1f;
             }
         }
