7357419976a9b9ed451735dfeec5cd0b05cc692e
PhamQuyen27 <2051063534@e.tlu.edu.vn>
2024-01-02 22:09:14 +0700

    Update game

---
+++ Assets/_Project/Scripts/EnemyFactory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Splines;
+
+namespace Shmup
+{
+    public class EnemyFactory 
+    {
+        public GameObject CreateEnemy (EnemyType enemytype, SplineContainer spline)
+        {
+            EnemyBuilder builder = new EnemyBuilder()
+                .SetBasePrefab(enemytype.enemyPrefab)
+                .SetSpline(spline)
+                .SetSpeed(enemytype.speed);
+
+            return builder.Build();
+        }
+
+    }
+}
