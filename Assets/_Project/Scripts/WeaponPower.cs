5d88fac45841e63f57ad295f9061c45336e588af
Hoàng Minh Quân <zedloikiem666@gmail.com>
2024-01-12 18:25:31 +0700

    update minion

---
+++ Assets/_Project/Scripts/WeaponPower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shmup
+{
+    public class WeaponPower : Item
+    {
+        public bool isCollected = false;
+        public GameObject helperPrefab;
+        public bool isHelperActive = false;
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                isCollected = true;
+                isHelperActive = true;
+            }
+        }
+    }
+}
---
+++ Assets/minion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shmup
+{
+    public class minion : WeaponPower
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Kiểm tra xem thuộc tính isHelperActive có phải là true hay không
+            if (isHelperActive)
+            {
+                // Đặt thuộc tính enabled của trợ thủ thành true
+                enabled = true;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
